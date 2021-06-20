e2ab862724990cf2bc888ad8f0f3cfa232b78100
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-06-20 12:02:49 +0530

    Part time wage calculated

--- empWageComputatiom.sh
+++ empWageComputatiom.sh
@@ -2,14 +2,18 @@
 
 echo "Welcome to empComputation"
 
-isPresent=1
-randomCheck=$((RANDOM%2))
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM%3))
 
-if [ $isPresent -eq $randomCheck ]
+if [ $isFullTime -eq $randomCheck ]
 then
-     empRatePerHr=20
      empHrs=8;
-     salary=$(( $empHrs * $empRatePerHr ))
+elif [ $isPartTime -eq $randomCheck ]
+then
+     empHrs=4;
 else
-    salary=0
+    empHrs=0;
 fi
+salary=$(( $empHrs * $empRatePerHr ))
